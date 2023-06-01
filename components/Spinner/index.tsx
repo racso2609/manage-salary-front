132e0ec976fd3d55a87857a23b3b27179edb5b7d
racso2609 <oscaremiliolugo@gmail.com>
2023-06-01 10:15:36 -0400

    change spinner

--- components/Spinner/index.tsx
+++ components/Spinner/index.tsx
@@ -1,54 +1,19 @@
 import { FC } from 'react';
-import { Animated } from 'react-native';
-import styled from 'styled-components/native';
-import useAnimatedColor from '../../hooks/animations/useAnimatedColor';
+import { ActivityIndicator } from 'react-native';
 import useColor from '../../hooks/useColors';
 
-const SpinnerContainer = styled.View`
-    width: 20px;
-    height: 20px;
-    margin: auto;
-`;
-
-const Spinner: FC = () => {
+interface propsType {
+    size?: 'small' | 'large';
+    color?: 'string';
+}
+const Spinner: FC = ({ size, color }: propsType) => {
     const { colors } = useColor();
-    const { colorAnimation: backgroundColorAnimated } = useAnimatedColor({
-        colorConfig: {
-            inputRange: [0, 100],
-            outputRange: [colors.hover, colors.ph],
-        },
-        handleAnimation: (animation: any) => {
-            const duration = 2000;
-            Animated.loop(
-                Animated.sequence([
-                    Animated.timing(animation, {
-                        toValue: 100,
-                        duration,
-                        useNativeDriver: false,
-                    }),
-                    Animated.timing(animation, {
-                        toValue: 0,
-                        duration,
-                        useNativeDriver: false,
-                    }),
-                ])
-            ).start();
-        },
-    });
 
     return (
-        <SpinnerContainer>
-            <Animated.View
-                style={{
-                    borderColor: backgroundColorAnimated,
-                    borderStyle: 'solid',
-                    borderWidth: 1,
-                    borderRadius: 20,
-                    width: '100%',
-                    height: '100%',
-                }}
-            ></Animated.View>
-        </SpinnerContainer>
+        <ActivityIndicator
+            size={size ?? 'small'}
+            color={color ?? colors.hover}
+        />
     );
 };
 
